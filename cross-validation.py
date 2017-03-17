c4b9695c6e2468f55bb1319aa11dc77b60a4504a
GBATZOLIS <gb511@cam.ac.uk>
2017-03-17 16:31:05 +0000

    I am giving up on you

--- cross-validation.py
+++ cross-validation.py
@@ -15,6 +15,16 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import GridSearchCV
 
+class MidpointNormalize(Normalize):
+
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
+    
 def supervised_classification(X,y):
     
 
